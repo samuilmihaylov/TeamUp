c4ada981bcca94acc4d18745d0b0b616562c674b
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-10-01 19:44:37 +0300

    DatePicker and TimePicker Dialogs added

---
+++ app/src/main/java/com/teamup/mihaylov/teamup/Events/CreateEvent/EventDatePickerFragment.java
@@ -0,0 +1,50 @@
+package com.teamup.mihaylov.teamup.Events.CreateEvent;
+
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.teamup.mihaylov.teamup.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventDatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), R.style.DialogTheme, this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar c = Calendar.getInstance();
+        c.set(year, month, day);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String formattedDate = dateFormat.format(c.getTime());
+
+        ((CreateEventActivity)getActivity()).setDate(formattedDate);
+
+        Intent intent = new Intent();
+        intent.putExtra("selected_date", formattedDate);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
+    }
+}
---
+++ app/src/main/java/com/teamup/mihaylov/teamup/Events/CreateEvent/EventTimePickerFragment.java
@@ -0,0 +1,53 @@
+package com.teamup.mihaylov.teamup.Events.CreateEvent;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.teamup.mihaylov.teamup.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventTimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), R.style.DialogTheme, this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm a");
+        String formattedTime = dateFormat.format(c.getTime());
+
+        ((CreateEventActivity)getActivity()).setTime(formattedTime);
+
+        Intent intent = new Intent();
+        intent.putExtra("selected_time", formattedTime);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
+    }
+}
