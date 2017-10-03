35359b20d79006892a99bb946cbdb55b00a16576
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-10-03 10:54:44 +0300

    Event model minor changes

--- app/src/main/java/com/teamup/mihaylov/teamup/base/models/Event.java
+++ app/src/main/java/com/teamup/mihaylov/teamup/base/models/Event.java
@@ -3,41 +3,47 @@ package com.teamup.mihaylov.teamup.base.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by samui on 29.9.2017 г..
  */
 
 public class Event implements Parcelable {
-    private String mName;
-    private String mDescription;
-    private String mDate;
-    private String mTime;
-    private String mAuthorName;
-    private String mAuthorId;
+    private String id;
+    private String name;
+    private String description;
+    private String date;
+    private String time;
+    private String authorName;
+    private String authorId;
+    private ArrayList<String> participants;
 
     public Event(){
 
     }
 
     public Event(Parcel in) {
-        mName = in.readString();
-        mDescription = in.readString();
-        mDate = in.readString();
-        mTime = in.readString();
-        mAuthorName = in.readString();
-        mAuthorId = in.readString();
+        id = in.readString();
+        name = in.readString();
+        description = in.readString();
+        date = in.readString();
+        time = in.readString();
+        authorName = in.readString();
+        authorId = in.readString();
+        participants = (ArrayList<String>) in.readSerializable();
     }
 
     @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(mName);
-        dest.writeString(mDescription);
-        dest.writeString(mDate);
-        dest.writeString(mTime);
-        dest.writeString(mAuthorName);
-        dest.writeString(mAuthorId);
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(id);
+        out.writeString(name);
+        out.writeString(description);
+        out.writeString(date);
+        out.writeString(time);
+        out.writeString(authorName);
+        out.writeString(authorId);
+        out.writeSerializable(participants);
     }
 
     public static final Parcelable.Creator<Event> CREATOR = new Parcelable.Creator<Event>(){
@@ -49,66 +55,103 @@ public class Event implements Parcelable {
         }
     };
 
-    public Event(String name, String description, String date, String time, String authorName, String authorId) {
+    public Event(
+            String id,
+            String name,
+            String description,
+            String date,
+            String time,
+            String authorName,
+            String authorId,
+            ArrayList<String> participants) {
+        setId(id);
         setName(name);
         setDescription(description);
         setDate(date);
         setTime(time);
         setAuthorName(authorName);
         setAuthorId(authorId);
+        setParticipants(participants);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
-        return this.mName;
+        return this.name;
     }
 
     public void setName(String name) {
-        this.mName = name;
+        this.name = name;
     }
 
     public String getDescription() {
-        return this.mDescription;
+        return this.description;
     }
 
     public void setDescription(String description) {
-        this.mDescription = description;
+        this.description = description;
     }
 
     public String getDate() {
-        return this.mDate;
+        return this.date;
     }
 
     public void setDate(String date) {
-        this.mDate = date;
+        this.date = date;
     }
 
     public String getTime() {
-        return this.mTime;
+        return this.time;
     }
 
     public void setTime(String time) {
-        this.mTime = time;
+        this.time = time;
     }
 
     public String getAuthorName() {
-        return this.mAuthorName;
+        return this.authorName;
     }
 
     public void setAuthorName(String mAuthorName) {
-        this.mAuthorName = mAuthorName;
+        this.authorName = mAuthorName;
     }
 
     public String getAuthorId() {
-        return this.mAuthorId;
+        return this.authorId;
     }
 
     public void setAuthorId(String mAuthorId) {
-        this.mAuthorId = mAuthorId;
+        this.authorId = mAuthorId;
+    }
+
+    public ArrayList<String> getParticipants(){
+        if(this.participants == null){
+            this.participants = new ArrayList<>();
+        }
+
+        return this.participants;
+    }
+
+    public void setParticipants(ArrayList<String> participants) {
+        this.participants = participants;
+    }
+
+    public void addParticipant(String id){
+        this.participants.add(id);
+    }
+
+    public void removeParticipant(String id){
+        this.participants.remove(id);
     }
 
     @Override
     public int describeContents() {
         return 0;
     }
-
 }
