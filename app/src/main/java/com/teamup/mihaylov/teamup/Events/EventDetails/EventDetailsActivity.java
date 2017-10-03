4a0d5d4062d58b5222d4288607cca4d16040a6e6
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-10-03 10:53:55 +0300

    Join/Leave event logic implemented

--- app/src/main/java/com/teamup/mihaylov/teamup/Events/EventDetails/EventDetailsActivity.java
+++ app/src/main/java/com/teamup/mihaylov/teamup/Events/EventDetails/EventDetailsActivity.java
@@ -2,6 +2,10 @@ package com.teamup.mihaylov.teamup.Events.EventDetails;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.teamup.mihaylov.teamup.DrawerNavMain.DrawerNavMainActivity;
 import com.teamup.mihaylov.teamup.R;
 import com.teamup.mihaylov.teamup.base.models.Event;
@@ -10,11 +14,18 @@ public class EventDetailsActivity extends DrawerNavMainActivity {
 
     private EventDetailsFragment mEventDetailsFragment;
     private Event mEvent;
+    private FirebaseUser mUser;
+    private String mEventId;
+    private DatabaseReference mDbEvensRef;
+    private String mUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mUserId = mUser.getUid();
 
         mEventDetailsFragment = new EventDetailsFragment();
 
@@ -23,10 +34,31 @@ public class EventDetailsActivity extends DrawerNavMainActivity {
                 .replace(R.id.content_container, mEventDetailsFragment)
                 .commit();
 
-        mEvent = (Event) getIntent().getParcelableExtra("eventDetails");
+        mEvent = (Event) getIntent().getParcelableExtra("event_details");
+        mEventId = mEvent.getId();
+
+        mDbEvensRef = FirebaseDatabase.getInstance().getReference("events");
     }
 
     public Event getEvent(){
         return mEvent;
+    }
+
+    public Boolean isAuthor() {
+        return this.mEvent.getAuthorId().equals(mUserId);
+    }
+
+    public Boolean isParticipating(){
+        return this.mEvent.getParticipants().contains(mUser.getUid());
+    }
+
+    public void joinEvent(){
+        this.mEvent.addParticipant(mUser.getUid());
+        mDbEvensRef.child(mEventId).setValue(this.mEvent);
+    }
+
+    public void leaveEvent(){
+        this.mEvent.removeParticipant(mUser.getUid());
+        mDbEvensRef.child(mEventId).setValue(mEvent);
     }
 }
--- app/src/main/java/com/teamup/mihaylov/teamup/Events/EventDetails/EventDetailsFragment.java
+++ app/src/main/java/com/teamup/mihaylov/teamup/Events/EventDetails/EventDetailsFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.teamup.mihaylov.teamup.R;
@@ -20,6 +21,35 @@ public class EventDetailsFragment extends Fragment {
     private TextView mEventName;
     private TextView mEventDescription;
 
+    private Button mBtnJoinEvent;
+    private Button mBtnLeaveEvent;
+    private Button mBtnEditEvent;
+
+    private Button.OnClickListener mBtnJoinEventListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            ((EventDetailsActivity) getActivity()).joinEvent();
+            mBtnJoinEvent.setVisibility(View.GONE);
+            mBtnLeaveEvent.setVisibility(View.VISIBLE);
+        }
+    };
+
+    private Button.OnClickListener mBtnEditEventListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
+
+    private Button.OnClickListener mBtnLeaveEventListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            ((EventDetailsActivity) getActivity()).leaveEvent();
+            mBtnJoinEvent.setVisibility(View.VISIBLE);
+            mBtnLeaveEvent.setVisibility(View.GONE);
+        }
+    };
+
     public EventDetailsFragment() {
         // Required empty public constructor
     }
@@ -27,15 +57,35 @@ public class EventDetailsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_event_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_event_details, container, false);
 
-        Event event = ((EventDetailsActivity)getActivity()).getEvent();
+        Event event = ((EventDetailsActivity) getActivity()).getEvent();
 
         mEventName = (TextView) view.findViewById(R.id.event_name);
         mEventDescription = (TextView) view.findViewById(R.id.event_description);
 
         mEventName.setText("Name: " + event.getName());
         mEventDescription.setText("Description: " + event.getDescription());
+
+        mBtnJoinEvent = (Button) view.findViewById(R.id.btn_join_event);
+        mBtnEditEvent = (Button) view.findViewById(R.id.btn_edit_event);
+        mBtnLeaveEvent = (Button) view.findViewById(R.id.btn_leave_event);
+
+        mBtnJoinEvent.setOnClickListener(mBtnJoinEventListener);
+        mBtnEditEvent.setOnClickListener(mBtnEditEventListener);
+        mBtnLeaveEvent.setOnClickListener(mBtnLeaveEventListener);
+
+        mBtnJoinEvent.setVisibility(View.GONE);
+        mBtnLeaveEvent.setVisibility(View.GONE);
+        mBtnEditEvent.setVisibility(View.GONE);
+
+        if (((EventDetailsActivity) getActivity()).isAuthor()) {
+            mBtnEditEvent.setVisibility(View.VISIBLE);
+        } else if (((EventDetailsActivity) getActivity()).isParticipating()) {
+            mBtnLeaveEvent.setVisibility(View.VISIBLE);
+        } else {
+            mBtnJoinEvent.setVisibility(View.VISIBLE);
+        }
 
         return view;
     }
