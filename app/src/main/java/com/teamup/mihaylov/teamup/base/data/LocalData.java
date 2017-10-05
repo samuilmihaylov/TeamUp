3a43cb5be278873843cea7ad604b388e7880647f
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-10-05 21:34:12 +0300

    Firebase getAll() onDataChange wrapped in Observable

--- app/src/main/java/com/teamup/mihaylov/teamup/base/data/LocalData.java
+++ app/src/main/java/com/teamup/mihaylov/teamup/base/data/LocalData.java
@@ -8,32 +8,5 @@ import io.reactivex.Observable;
  * Created by samui on 3.10.2017 г..
  */
 
-public class LocalData<T> extends BaseData<T> {
-
-    private final ArrayList<T> items;
-
-    public LocalData() {
-        this.items = new ArrayList<T>();
-    }
-
-    @Override
-    public Observable<T[]> getAll() {
-        return Observable.just((T[]) this.items.toArray());
-    }
-
-    @Override
-    public Observable<T> getById(String id) {
-        return Observable.just(null);
-    }
-
-    @Override
-    public void add(T item) {
-        this.items.add(item);
-    }
-
-    @Override
-    public Observable remove(T item) {
-        this.items.remove(item);
-        return Observable.just(item);
-    }
+public class LocalData<T> {
 }
