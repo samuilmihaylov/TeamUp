f08bb7d4e4e5f2f7fe6b3bbf9c3c55774cdd0dc5
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-10-05 21:19:37 +0300

    Revert "AuthenticationProvider getAll() Firebase onDataChange() wrapped in Observable"
    This reverts commit 563a9ddda4b392228e9def0b9539f79efb322769.

--- app/src/main/java/com/teamup/mihaylov/teamup/base/authentication/AuthenticationProvider.java
+++ app/src/main/java/com/teamup/mihaylov/teamup/base/authentication/AuthenticationProvider.java
@@ -5,10 +5,6 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -22,36 +18,29 @@ import javax.inject.Inject;
  * Created by samui on 3.10.2017 г..
  */
 
-public class AuthenticationProvider implements FirebaseAuth.AuthStateListener, GoogleApiClient.OnConnectionFailedListener{
+public class AuthenticationProvider {
 
     private FirebaseUser mUser;
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     private Context mContext;
     private Activity mActivity;
-    private GoogleApiClient mGoogleApiClient;
 
     @Inject
     AuthenticationProvider(Context context) {
         mContext = context;
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("695242278170-tn551iph1cuf7jpvbpl2tl4c51b1ufaq.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(mContext)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
         mAuth = FirebaseAuth.getInstance();
 
-        mAuth.addAuthStateListener(this);
-    }
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mUser = firebaseAuth.getCurrentUser();
+            }
+        };
 
-    @Override
-    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        mUser = firebaseAuth.getCurrentUser();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     public FirebaseAuth getFirebaseInstance() {
@@ -72,10 +61,6 @@ public class AuthenticationProvider implements FirebaseAuth.AuthStateListener, G
 
     public String getUserDisplayName() {
         return mAuth.getCurrentUser().getDisplayName();
-    }
-
-    public GoogleApiClient getGoogleApi(){
-        return mGoogleApiClient;
     }
 
     public void updateUserProfile(String displayName) {
@@ -117,64 +102,8 @@ public class AuthenticationProvider implements FirebaseAuth.AuthStateListener, G
                 });
     }
 
-    public void changeEmail(String newEmail) {
-        mUser.updateEmail(newEmail.trim())
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(mContext, "Email address is updated. Please sign in with the new email address!", Toast.LENGTH_LONG).show();
-                            signOut();
-                        } else {
-                            Toast.makeText(mContext, "Failed to update email!", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-    }
-
-    public void changePassword(String newPassword) {
-        mUser.updatePassword(newPassword.trim())
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(mContext, "Password is updated, sign in with new password!", Toast.LENGTH_SHORT).show();
-                            signOut();
-                        } else {
-                            Toast.makeText(mContext, "Failed to update password!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-    }
-
-    public void sendPasswordResetEmail(String email) {
-        mAuth.sendPasswordResetEmail(email.trim())
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(mContext, "Reset password email is sent!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(mContext, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
     public void signOut() {
         mAuth.signOut();
         Toast.makeText(mContext, "Signed Out", Toast.LENGTH_SHORT).show();
-    }
-
-    public void googleSignOut(){
-        signOut();
-        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient);
-        Toast.makeText(mContext, "Signed Out", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(mContext, "onConnectionFailed" + connectionResult, Toast.LENGTH_SHORT).show();
     }
 }
